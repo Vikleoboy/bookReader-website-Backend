ff14c5b3839e10a493f0dd966b814508e783ef3c
HoussemDegachi <houssemdegach@gmail.com>
2024-07-03 08:56:06 +0200

    added auth + security improvements

--- schemas/Mail.js
+++ schemas/Mail.js
@@ -3,7 +3,7 @@ import Joi from "./Joi.js"
 export const email = Joi.object({
     username: Joi.string().min(2).required(),
     email: Joi.string().email().required(),
-    role: Joi.string().valid("user", "dev").required(),
+    role: Joi.string().valid("user", "tester").required(),
 }).required()
 
 export default {
